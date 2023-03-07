4c9a40b5499e2eb09661aacbb0287cb772f54134
Ashni Chinnu Sam <89782765+ashnichinnusam@users.noreply.github.com>
2023-03-07 09:44:50 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+$(".input_text").focus(function(){
+    $(this).prev('.fa').addclass('glowIcon')
+})
+$(".input_text").focusout(function(){
+    $(this).prev('.fa').removeclass('glowIcon')
+})
+
+function change(){
+    window.location.href="index.html"
+}
